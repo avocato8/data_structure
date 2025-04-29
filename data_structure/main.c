825ba1dd2224252f3ceb2dbf8f4fd4373de0fb3b
avocato <nightfury805@gmail.com>
2025-04-30 01:18:47 +0900

    Initial Commit

---
+++ data_structure/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  data_structure
+//
+//  Created by gisung's Mac on 4/30/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
